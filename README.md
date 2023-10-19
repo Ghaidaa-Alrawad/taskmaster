f548260b31576e183ff55341cf5bb96d942cfdbf
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-10-19 22:20:58 +0000

    Lab 27

--- README.md
+++ README.md
@@ -4,12 +4,21 @@ This mobile app revolves around seamlessly adding tasks and effortlessly viewing
 
 ---
 
+## What is new??
+
+- Tasks Details page
+- You can add you name to be displayed in the home page form the setting page 
+
 ## Application screenshots
 
-![HomePage](/screenshots/homePage.jpeg)
+![HomePage](/screenshots/HomePage.jpeg)
 
 ![AddTaskPage](/screenshots/addTaskPage.jpeg)
 
 ![AddTaskSubmitButton](/screenshots/addTaskSubmitbutton.jpeg)
 
 ![AllTasksPage](/screenshots/allTasksPage.jpeg)
+
+![DetailsPage](/screenshots/detailsPage.jpeg)
+
+![SettingPage](/screenshots/settingPage.jpeg)
--- app/src/main/java/com/tasks/taskmanager/activity/MainActivity.java
+++ app/src/main/java/com/tasks/taskmanager/activity/MainActivity.java
@@ -3,9 +3,11 @@ package com.tasks.taskmanager.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tasks.taskmanager.R;
@@ -17,26 +19,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addTask = (Button) findViewById(R.id.addTaskButton);
+        Button addTask = findViewById(R.id.addTaskButton);
 
-        addTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("welcome to add task page");
-
-                Intent goToAddTaskPage = new Intent(MainActivity.this, AddTask.class);
-                startActivity(goToAddTaskPage);
-            }
+        addTask.setOnClickListener(v -> {
+            Intent goToAddTaskPage = new Intent(MainActivity.this, AddTask.class);
+            startActivity(goToAddTaskPage);
         });
 
-        Button allTasks = (Button) findViewById(R.id.allTasksButton);
+        Button allTasks = findViewById(R.id.allTasksButton);
 
-        allTasks.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToAllTasksPage = new Intent(MainActivity.this, AllTasks.class);
-                startActivity(goToAllTasksPage);
-            }
+        allTasks.setOnClickListener(v -> {
+            Intent goToAllTasksPage = new Intent(MainActivity.this, AllTasks.class);
+            startActivity(goToAllTasksPage);
         });
+
+        Button taskButton1 = findViewById(R.id.taskButton1);
+        Button taskButton2 = findViewById(R.id.taskButton2);
+        Button taskButton3 = findViewById(R.id.taskButton3);
+
+        taskButton1.setOnClickListener(v -> openTaskDetails("Task 1"));
+        taskButton2.setOnClickListener(v -> openTaskDetails("Task 2"));
+        taskButton3.setOnClickListener(v -> openTaskDetails("Task 3"));
+
+        Button settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(v -> {
+            Intent goToSettings = new Intent(MainActivity.this, Settings.class);
+            startActivityForResult(goToSettings, 1);
+        });
+
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String username = prefs.getString("username", "");
+
+        TextView usernameTextView = findViewById(R.id.usernameTextView);
+        usernameTextView.setText(username + "'s tasks");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String username = data.getStringExtra("username");
+
+            TextView usernameTextView = findViewById(R.id.usernameTextView);
+            usernameTextView.setText(username + "'s tasks");
+        }
+    }
+
+    private void openTaskDetails(String taskTitle) {
+        Intent goToTaskDetails = new Intent(MainActivity.this, TaskDetails.class);
+        goToTaskDetails.putExtra("taskTitle", taskTitle);
+        startActivity(goToTaskDetails);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String username = prefs.getString("username", "");
+
+        TextView usernameTextView = findViewById(R.id.usernameTextView);
+        usernameTextView.setText(username + "'s tasks");
     }
 }
